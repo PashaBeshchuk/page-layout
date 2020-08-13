45fdae514fdc891feb1db5d255d13ac91a2d2f65
Pasha Beshchuk <pashabeshchuk1990@gmail.com>
2020-08-14 00:19:44 +0300

    Updates

---
+++ precache-manifest.9d861b2df7de9d4c1c30b5e66c67d43a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f424f8686149828f16d3c6908eb392e",
+    "url": "/page-layout/index.html"
+  },
+  {
+    "revision": "a0d3a0061f98eb590c7c",
+    "url": "/page-layout/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "7bad6a8250677a4fa8bf",
+    "url": "/page-layout/static/css/main.c4f0f3a5.chunk.css"
+  },
+  {
+    "revision": "a0d3a0061f98eb590c7c",
+    "url": "/page-layout/static/js/2.0e54b74c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/page-layout/static/js/2.0e54b74c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bad6a8250677a4fa8bf",
+    "url": "/page-layout/static/js/main.86a5c8f4.chunk.js"
+  },
+  {
+    "revision": "b39eac1cd73d772a096d",
+    "url": "/page-layout/static/js/runtime-main.db207638.js"
+  },
+  {
+    "revision": "589c020a0e0542bee01841c977478f78",
+    "url": "/page-layout/static/media/Vector.589c020a.jpg"
+  },
+  {
+    "revision": "8d42a7d271671d70a5aca6688077f92c",
+    "url": "/page-layout/static/media/header.8d42a7d2.jpg"
+  },
+  {
+    "revision": "833c2f92e0d10074fbb587f97bd86812",
+    "url": "/page-layout/static/media/image-face.833c2f92.jpg"
+  },
+  {
+    "revision": "8034d8649569e381c4223654e26b694a",
+    "url": "/page-layout/static/media/image-mass.8034d864.jpg"
+  },
+  {
+    "revision": "2f98ade0bf3ed41bfbb2f2332fb811c4",
+    "url": "/page-layout/static/media/image-reu.2f98ade0.jpg"
+  }
+]);
